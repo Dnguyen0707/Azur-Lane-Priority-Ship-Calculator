6709ac0c29bc7ae5706783766589ec507a13effb
Dai Nguyen <dainguyen0707@gmail.com>
2020-03-29 19:56:19 -0700

    Changed retrofit

--- src/Reader.java
+++ src/Reader.java
@@ -6,7 +6,6 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.FileInputStream;
 import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.stream.Collectors;
